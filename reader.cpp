29db20f62af949dd595cd20f11abde766ef6a181
Arles96 <arles.cerrato@unitec.edu>
2018-09-18 11:51:48 -0600

    Finalizacion del escritor

--- reader.cpp
+++ reader.cpp
@@ -4,16 +4,23 @@
 #include <semaphore.h>
 #include <vector>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 using namespace std;
 
+#define STRING_SIZE 20
+#define TABLE_SIZE 50
+
 struct table {
-  string client;
   int orden;
-  string date;
   int products;
   double total;
+  char client[STRING_SIZE];
+  char date [STRING_SIZE] ;
 };
 
 int getIntRandom (int range) {
@@ -28,9 +35,20 @@ double getDoubleRandom (int range, double min, double max) {
 }
 
 struct sharedMemory {
-  vector<table> array;
+  table array [TABLE_SIZE];
+  int counter = 0;
+  sem_t mutex;
 };
 
+sharedMemory *memory;
+
 int main () {
+  string clients [5] = { "Dario", "Arles", "Cristhian", "Harold", "Ana" };
+  key_t key = 5646;
+  int shmid;
+  if ((shmid = shmget(key, sizeof(struct sharedMemory), 0644 | IPC_CREAT)) == -1) {
+    perror("shmget");
+    exit(1);
+  }
   return 0;
 }
--- writer.cpp
+++ writer.cpp
@@ -4,16 +4,25 @@
 #include <semaphore.h>
 #include <vector>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <ctime>
 
 using namespace std;
 
+#define STRING_SIZE 20
+#define TABLE_SIZE 50
+
 struct table {
-  string client;
   int orden;
-  string date;
   int products;
   double total;
+  char client[STRING_SIZE];
+  char date [STRING_SIZE] ;
 };
 
 int getIntRandom (int range) {
@@ -28,11 +37,45 @@ double getDoubleRandom (int range, double min, double max) {
 }
 
 struct sharedMemory {
-  vector<table> array;
+  table array [TABLE_SIZE];
+  int counter = 0;
   sem_t mutex;
 };
 
+sharedMemory *memory;
+char clients [5] [STRING_SIZE] = { "Dario", "Arles", "Cristhian", "Harold", "Ana" };
+
 int main () {
-  string clients [5] = { "Dario", "Arles", "Cristhian", "Harold", "Ana" };
+  key_t key = 5646;
+  int shmid;
+  if ((shmid = shmget(key, sizeof(struct sharedMemory), 0644 | IPC_CREAT)) == -1) {
+    perror("shmget");
+    exit(1);
+  }
+  memory = (sharedMemory *)shmat(shmid, NULL, 0);
+  if (memory == (sharedMemory *)(-1)) {
+    perror("shmat");
+    exit(1);
+  }
+  while (true) {
+    int option = getIntRandom(2);
+    if (option == 0) { // Agregando ordenes
+      table *newTable;
+      strcpy(newTable->client, clients[getIntRandom(5)]);
+      newTable->orden = getIntRandom(2019);
+      time_t now = time(0);
+      strcpy(newTable->date, ctime(&now));
+      newTable->products = getIntRandom(20);
+      newTable->total = getDoubleRandom(500, 100, 499);
+      memory->array[memory->counter] = *newTable;
+      memory->counter++;
+      cout << "Un escritor agrego una orden" << endl;
+    } else { // eliminando ordenes
+      memory->array[memory->counter] = NULL;
+      memory->counter--;
+      cout << "Un escritor ha eliminado una orden" << endl;
+    }
+    sleep(2);
+  }
   return 0;
 }
